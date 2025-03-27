a13ca1a3543a99e947b5d1e2dbf04fc40a114805
ravilock <ravi.me.professional@hotmail.com>
2025-03-27 11:10:12 -0300

    chore: add copyright comment on go files

--- pkg/rate-limit/rate-limit.go
+++ pkg/rate-limit/rate-limit.go
@@ -1,3 +1,7 @@
+// Copyright 2025 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package ratelimit
 
 // TODO: Make a comment noting that this references the fields from nginx rate limit typing
