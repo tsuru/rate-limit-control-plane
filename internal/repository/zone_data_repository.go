187f43f539733ded3a7fa8a00b93c792b39351ab
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2025-06-03 16:44:34 -0300

    feat: implement ZoneDataRepository for managing RPAAS zone data

---
+++ internal/repository/zone_data_repository.go
@@ -0,0 +1,91 @@
+package repository
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"slices"
+	"sync"
+
+	"github.com/tsuru/rate-limit-control-plane/internal/config"
+	"github.com/tsuru/rate-limit-control-plane/internal/logger"
+	"github.com/tsuru/rate-limit-control-plane/internal/ratelimit"
+)
+
+type ZoneDataRepository struct {
+	sync.Mutex
+	logger                *slog.Logger
+	Data                  map[string][]byte
+	readRpaasZoneDataChan chan ratelimit.RpaasZoneData
+}
+
+func NewRpaasZoneDataRepository() (*ZoneDataRepository, chan ratelimit.RpaasZoneData) {
+	readRpaasZoneDataChan := make(chan ratelimit.RpaasZoneData)
+	repositoryLogger := logger.NewLogger(map[string]string{"emitter": "rate-limit-control-plane-repository"}, os.Stdout)
+	zoneRepository := &ZoneDataRepository{
+		logger:                repositoryLogger,
+		Data:                  make(map[string][]byte),
+		readRpaasZoneDataChan: readRpaasZoneDataChan,
+	}
+	go zoneRepository.startReader()
+
+	return zoneRepository, readRpaasZoneDataChan
+}
+
+func (z *ZoneDataRepository) startReader() {
+	for rpaasZoneData := range z.readRpaasZoneDataChan {
+		z.insert(rpaasZoneData)
+	}
+}
+
+func (z *ZoneDataRepository) insert(rpaasZoneData ratelimit.RpaasZoneData) {
+	z.Lock()
+	serverData := []Data{}
+	for _, zone := range rpaasZoneData.Data {
+		for _, entry := range zone.RateLimitEntries {
+			serverData = append(serverData, Data{
+				ID:     fmt.Sprintf("%s:%s", entry.Key.String(zone.RateLimitHeader), zone.Name),
+				Last:   entry.Last,
+				Excess: entry.Excess,
+			})
+		}
+	}
+	slices.SortFunc(serverData, func(a, b Data) int {
+		if a.Excess < b.Excess {
+			return 1
+		}
+		if a.Excess > b.Excess {
+			return -1
+		}
+		return 0
+	})
+	if len(serverData) > config.Spec.MaxTopOffensorsReport {
+		serverData = serverData[:config.Spec.MaxTopOffensorsReport]
+	}
+	dataBytes, err := json.MarshalIndent(serverData, "  ", "  ")
+	if err != nil {
+		z.logger.Error("Error marshaling JSON", "error", err)
+		z.Unlock()
+		return
+	}
+	z.Data[rpaasZoneData.RpaasName] = dataBytes
+	z.Unlock()
+}
+
+func (z *ZoneDataRepository) GetRpaasZoneData(rpaasName string) ([]byte, bool) {
+	z.Lock()
+	defer z.Unlock()
+	dataBytes, exists := z.Data[rpaasName]
+	return dataBytes, exists
+}
+
+func (z *ZoneDataRepository) ListInstances() []string {
+	z.Lock()
+	defer z.Unlock()
+	instance := make([]string, 0, len(z.Data))
+	for key := range z.Data {
+		instance = append(instance, key)
+	}
+	return instance
+}
